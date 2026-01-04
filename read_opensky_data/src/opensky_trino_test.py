19ae784251ce060de0b839b9a9846c6f390a6926
David Kirchner <140542671+davidkirch@users.noreply.github.com>
2026-01-04 16:29:59 +0100

    chore: removed stagnent folders

--- read_opensky_data/src/opensky_trino_test.py
+++
@@ -1,9 +0,0 @@
-from pyopensky.trino import Trino
-
-trino = Trino()
-
-# Get detailed trajectory data (state vectors)
-trino.history(
-    start="2019-11-01 09:00",
-    stop="2019-11-01 12:00",
-)
